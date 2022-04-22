ed3fa76b8b6e8f37b38b82992517508969a0af80
Thendez1523 <102190814+Thendez1523@users.noreply.github.com>
2022-04-22 17:25:59 -0600

    Add files via upload

---
+++ sumaconfin.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main ()
+{
+    int a,v1,v2,r;
+while(a!=0)
+{
+    cout<<"Escriba 0 para salir"<<endl;
+    cin>>a;
+    cout<<"escriba un valor"<<endl;
+cin>> v1;
+cout<<"escriba el segundo valor"<<endl;
+cin>>v2;
+r=v1+v2;
+cout<<"La suma resultante es de: ";
+cout<<r<<endl;
+
+}
+
+return 0;
+
+}
